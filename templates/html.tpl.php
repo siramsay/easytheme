4be6ae171486b36cdfb4a73b1bfde9606d89adf6
siramsay <simon.ramsay@gmail.com>
2015-12-19 12:11:07 +0900

    add template folder& html5 tpl

---
+++ templates/html.tpl.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php print $head; ?>
+<title><?php print $head_title; ?></title>
+<?php print $styles; ?>
+<?php print $scripts; ?>
+<!-- TO BE REMOVE OR DOCUMENTED <meta name="google-site-verification" content="HwgKmHyMxsPmP1lRDnCzZaJPduluMDKXAKT3pnqdKp8" /> -->
+</head>
+<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
