8e985746db71e9ebcc57b801284b4ff979aa5cde
siramsay <simon.ramsay@gmail.com>
2016-05-15 19:12:15 +0900

    add some layouts to separate file to use later

---
+++ templates/layout-modules.tpl.php
@@ -0,0 +1,28 @@
+<div class="container">
+<div class="row testimonial">
+<!--<h2>What our current owners say</h2>-->
+<div class="fourcol" >
+“best best” <span>service ever</span>
+</div>
+<div class="fourcol" >
+“best best”
+</div>
+<div class="fourcol last" >
+“best best” <span>just for being</span>
+</div>
+</div>
+</div>
+
+<div class="container">
+<div class="row statistics">
+<div class="fourcol_stat" >
+<span>21</span>New Products
+</div>
+<div class="fourcol_stat" >
+<span>73</span>Hours Worked
+</div>
+<div class="fourcol_stat last" >
+<span>334</span>Staff
+</div>
+</div>
+</div>
